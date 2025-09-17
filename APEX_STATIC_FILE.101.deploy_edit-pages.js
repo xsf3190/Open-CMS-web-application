5568bbd60faa60044a6cc0ee159e1aa8347147fb
mark brown <mark.russellbrown@gmail.com>
2025-09-17 22:00:22 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edit-pages.js
+++ APEX_STATIC_FILE.101.deploy_edit-pages.js
@@ -1,25 +1,40 @@
 /*
 **  ADD / CHANGE / DELETE PAGES
 */
-import { nav, dropdown } from "deploy_elements";
+import { output_dialog, dialog_header, dialog_article, dialog_footer } from "deploy_elements";
 import { callAPI, handleError } from "deploy_callAPI";
 
-const nav_items = nav.querySelector("nav>ul");
-const editor = nav.nextElementSibling;
+let nav_items;
 
-let endpoint;
-
-export const init = (element) => {
-    endpoint = element.dataset.endpoint;
+const endpoint = "edit-pages/:ID/:PAGE";
+export const init = () => {
     callAPI(endpoint,'GET')
         .then((data) => {
-            editor.insertAdjacentHTML('afterbegin',data.html);
-            editor.scrollIntoView({ behavior: 'smooth', block: 'end' });
-            const edit = editor.querySelector("input[name='navigation_label']");
+            dialog_header.replaceChildren();
+            dialog_header.insertAdjacentHTML('afterbegin',data.header);
+            dialog_article.replaceChildren();
+            dialog_article.insertAdjacentHTML('afterbegin',data.article);
+            dialog_footer.replaceChildren();
+            dialog_footer.insertAdjacentHTML('afterbegin',data.footer);
+            output_dialog.showModal();
+
+            nav_items = dialog_article.querySelector("nav>ul");
+
+            const edit = dialog_article.querySelector("input[name='navigation_label']");
             const current = nav_items.querySelector("[aria-current='page']")
             edit.value = current.textContent;
             setCollectionType(current.dataset.collection);
             nav_items.querySelector(".visually-hidden")?.classList.remove("visually-hidden");
+
+            nav_items.addEventListener("click", (e) => {
+                e.preventDefault();
+                removeAriaCurrent();
+                e.target.setAttribute("aria-current","page");
+                const edit = dialog_article.querySelector("input[name='navigation_label']");
+                edit.value = e.target.textContent;
+                setCollectionType(e.target.dataset.collection);
+            })
+
         })
         .catch((error) => {
             handleError(error);
@@ -29,13 +44,13 @@ export const init = (element) => {
 let tmp = 0;
 
 const removeAriaCurrent = () => {
-  nav.querySelectorAll("a").forEach( (a) => {
+  nav_items.querySelectorAll("a").forEach( (a) => {
     a.removeAttribute("aria-current");
   })
 }
 
 const setCollectionType = (data) => {
-    const collection = editor.querySelectorAll("[name='collection_type']");
+    const collection = dialog_article.querySelectorAll("[name='collection_type']");
     switch (data) {
         case "N/A":
             collection[0].checked = true;
@@ -50,36 +65,24 @@ const setCollectionType = (data) => {
 }
 
 /*
-** USER CLICKS A NAVIGATION LABEL WHICH WE USE TO CAPTURE ANY CHANGES (LABEL TEXT, COLLECTION_TYPE)
-*/
-nav_items.addEventListener("click", (e) => {
-  e.preventDefault();
-  removeAriaCurrent();
-  e.target.setAttribute("aria-current","page");
-  const edit = editor.querySelector("input[name='navigation_label']");
-  edit.value = e.target.textContent;
-  setCollectionType(e.target.dataset.collection);
-})
-
-/*
 ** REFLECT CHANGES IMMEDIATELY IN NAV ELEMENTS
 */
-editor.addEventListener("input", (e) => {
+dialog_article.addEventListener("input", (e) => {
     if (e.target.matches("[name='navigation_label']")) {
-        nav.querySelector("[aria-current='page']").textContent = e.target.value;
+        nav_items.querySelector("[aria-current='page']").textContent = e.target.value;
     }
 
     if (e.target.matches("[name='collection_type']")) {
-        nav.querySelector("[aria-current='page']").dataset.collection = e.target.value;
+        nav_items.querySelector("[aria-current='page']").dataset.collection = e.target.value;
     }
 })
 
 /*
 ** BUTTON CLICK EVENT HANDLERS
 */
-editor.addEventListener("click", async (e) => {
+dialog_article.addEventListener("click", async (e) => {
     if (e.target.matches(".add-page")) {
-        const target = nav.querySelector("[aria-current='page']").parentElement;  // returns <li>
+        const target = nav_items.querySelector("[aria-current='page']").parentElement;  // returns <li>
         removeAriaCurrent();
         const clone = target.cloneNode(true);
         clone.firstChild.textContent = "[NEW PAGE]";
@@ -88,7 +91,7 @@ editor.addEventListener("click", async (e) => {
         clone.firstChild.setAttribute("aria-current","page");
         
         nav_items.insertBefore(clone, target.nextSibling);
-        const edit = editor.querySelector("input[name='navigation_label']");
+        const edit = dialog_article.querySelector("input[name='navigation_label']");
         edit.value = "[NEW PAGE]";
         return;
     }
@@ -112,43 +115,15 @@ editor.addEventListener("click", async (e) => {
             e.target.textContent = "Delete Site";
             return;
         }
-        const target = nav.querySelector("[aria-current='page']").parentElement;
+        const target = nav_items.querySelector("[aria-current='page']").parentElement;
         if (target.nextElementSibling) {
             target.nextElementSibling.firstChild.setAttribute("aria-current","page");
         } else {
             target.previousElementSibling.firstChild.setAttribute("aria-current","page");
         }
-        const edit = editor.querySelector("input[name='navigation_label']");
+        const edit = dialog_article.querySelector("input[name='navigation_label']");
         edit.value = nav_items.querySelector("[aria-current='page']").textContent;
         target.remove();
         return;
-    }
-
-    if (e.target.matches(".cancel-changes")) {
-        window.location.reload();
-        return;
-    }
-
-    if (e.target.matches(".publish-changes")) {
-        const arr = [];
-        nav_items.querySelectorAll("a").forEach ((item) => {
-            const obj = {};
-            obj.article_id = item.dataset.id;
-            obj.collection_type = item.dataset.collection;
-            obj.navigation_label = item.textContent;
-            arr.push(obj);
-        })
-        await callAPI(endpoint,'POST', arr)
-            .then((data) => {
-                console.log("data",data);
-                if (data.deleted>0) {
-                    window.history.replaceState({}, "", "index.html");
-                }
-            })
-            .catch((error) => {
-                handleError(error);
-            });
-
-        dropdown.querySelector("button.publish-website").click();
     }
 })
