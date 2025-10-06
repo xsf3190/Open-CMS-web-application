6443ec992fcfa5c0be60e31e4f5b02ff4aa32234
mark brown <mark.russellbrown@gmail.com>
2025-10-06 08:20:52 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_start.js
+++ APEX_STATIC_FILE.101.deploy_start.js
@@ -1,8 +1,6 @@
-window.addEventListener("load", () => {
-    // window.requestIdleCallback(() => {
-        const script = document.createElement("script");
-        script.src = "https://es-modules.netlify.app/test/javascript/deploy_main.min.js";
-        script.type = "module";
-        document.head.appendChild(script);
-    // });
-});
+document.addEventListener('DOMContentLoaded',()=>{
+    const script = document.createElement('script');
+    script.src = "/javascript/deploy_main.min.js";
+    script.type = "module";
+    document.body.appendChild(script);
+})
