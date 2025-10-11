b8d879f75cc7d3ecf23b2e388b0c649c2796271a
mark brown <mark.russellbrown@gmail.com>
2025-10-11 23:00:29 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_publish-website.js
+++ APEX_STATIC_FILE.101.deploy_publish-website.js
@@ -3,25 +3,21 @@
 */
 
 import { callAPI, handleError } from "deploy_callAPI";
-import { output_dialog } from "deploy_elements";
-
-const header = output_dialog.querySelector("header");
-const content = output_dialog.querySelector("article");
-const footer = output_dialog.querySelector("footer");
+import { output_dialog, dialog_header, dialog_article, dialog_footer } from "deploy_elements";
 
 const endpoint = "publish-website/:ID";
 let intervalId;
 
-export const init = (element) => {
+export const init = (e) => {
 
     sessionStorage.removeItem("pages_edited");
 
-    callAPI(endpoint,"POST",{})
+    callAPI(endpoint,"POST",{env:e.dataset.env})
         .then( (data) => {
-            content.replaceChildren();
-            content.insertAdjacentHTML('afterbegin',data.content);
-            header.querySelector(":first-child").replaceChildren();
-            footer.replaceChildren();
+            dialog_article.replaceChildren();
+            dialog_article.insertAdjacentHTML('afterbegin',data.content);
+            dialog_header.querySelector(":first-child").replaceChildren();
+            dialog_footer.replaceChildren();
             output_dialog.showModal();
             if (data.stop) return;
             if (intervalId) {
@@ -44,7 +40,7 @@ const getDeploymentStatus = () => {
     callAPI(endpoint,"GET")
         .then( (data) => {
             if (data.content) {
-                const tbody = content.querySelector("tbody");
+                const tbody = dialog_article.querySelector("tbody");
                 tbody.insertAdjacentHTML('beforeend',data.content);
                 tbody.querySelector(":last-child").scrollIntoView();
             }
