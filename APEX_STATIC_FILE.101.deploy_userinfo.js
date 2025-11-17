ff81e3816e6c0acded315c8e70ed5b119e9bc930
mark brown <mark.russellbrown@gmail.com>
2025-11-17 21:00:37 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

---
+++ APEX_STATIC_FILE.101.deploy_userinfo.js
@@ -0,0 +1,34 @@
+/*
+**  USER INFO MODULE - REVIEW LOGGED ON USER DETAILS, LOGOUT BUTTON HANDLER
+*/
+import { dialog_article, dialog_footer, initDialog, dropdown } from "deploy_elements";
+import { callAPI, handleError } from "deploy_callAPI";
+
+const selectHandler = async (e) => {
+    /* click on <button class="logout"> */
+    const live=dialog_footer.querySelector("[aria-live]");
+    if (e.target.classList.contains("logout")) {
+        callAPI("userinfo/:ID","DELETE",{action:"logout"})
+            .then((data) => {
+                live.textContent = "Logged out";
+                sessionStorage.clear();
+                localStorage.clear();
+                dropdown.replaceChildren();
+                dropdown.insertAdjacentHTML('afterbegin',data.dropdown);
+            })
+            .catch((error) => {
+                handleError(error);
+            });
+    }
+};
+
+export const init = () => {
+    callAPI("userinfo/:ID","GET")
+        .then((data) => {
+            initDialog(data);
+            dialog_article.addEventListener("click", selectHandler);
+        })
+        .catch((error) => {
+            handleError(error);
+        });
+}
