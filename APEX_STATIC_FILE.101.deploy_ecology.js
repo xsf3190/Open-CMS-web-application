35d94130d9e41097f6b8e070ba5e2c51736b1343
mark brown <mark.russellbrown@gmail.com>
2025-11-19 21:00:14 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

---
+++ APEX_STATIC_FILE.101.deploy_ecology.js
@@ -0,0 +1,21 @@
+/*
+**  ECOLOGY MODULE - SEND METRICS TO UNPROTECTED ENDPOINT
+*/
+import { bodydata, initDialog } from "deploy_elements";
+
+export const init = async (e) => {
+
+    const url = bodydata.resturl + e.dataset.endpoint.replace(":ID",bodydata.websiteid).replace(":PAGE",bodydata.articleid);
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    
+    let config = {method: e.dataset.method, headers: headers};
+    const lcp = sessionStorage.getItem("LCP");
+    const metrics = JSON.parse(sessionStorage.getItem("metrics"));
+    config["body"] = JSON.stringify({metrics:metrics,lcp:lcp});
+
+    const response = await fetch(url, config);
+    const result = await response.json();
+
+    initDialog(result);
+}
