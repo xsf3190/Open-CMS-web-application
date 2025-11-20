9cf3e16764386f3ebea335a358fd9d86c3411dff
mark brown <mark.russellbrown@gmail.com>
2025-11-20 21:00:22 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edited-content.js
+++ APEX_STATIC_FILE.101.deploy_edited-content.js
@@ -1,19 +1,25 @@
 /*
-**  FETCH LATEST CONTENT WHEN USER REVISITS EDITED PAGE - 
+**  FETCH LATEST CONTENT WHEN OWNER VISITS EDITED PAGE - 
 */
 import { header, main, footer, dropdown } from "deploy_elements";
 import { callAPI, handleError } from "deploy_callAPI";
 
 export const init = () => {
+    console.log("fetching latest edited content");
     callAPI('edit-content/:ID/:PAGE',"GET")
         .then((data) => {
-            header.replaceChildren();
-            header.insertAdjacentHTML("beforeend",data.header);
-            main.replaceChildren();
-            main.insertAdjacentHTML("beforeend",data.main);
-            footer.replaceChildren();
-            footer.insertAdjacentHTML("beforeend",data.footer);
-            dropdown.querySelector(".edit-content").click();
+            if (data.header) {
+                header.replaceChildren();
+                header.insertAdjacentHTML("beforeend",data.header);
+            }
+            if (data.main) {
+                main.replaceChildren();
+                main.insertAdjacentHTML("beforeend",data.main);
+            }
+            if (data.footer) {
+                footer.replaceChildren();
+                footer.insertAdjacentHTML("beforeend",data.footer);
+            }  
         })
         .catch((error) => {
             handleError(error);
