abb0c3dfdb8b5fd2b13b78931b6dd95be23e2569
mark brown <mark.russellbrown@gmail.com>
2025-11-24 21:00:34 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_start.js
+++ APEX_STATIC_FILE.101.deploy_start.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    document.body.dataset.jslib = "#JSLIB#";
+    document.body.dataset.resturl = "#RESTURL#";
     const script = document.createElement('script');
     script.src = document.body.dataset.jslib + "javascript/deploy_main.min.js";
     script.type = "module";
