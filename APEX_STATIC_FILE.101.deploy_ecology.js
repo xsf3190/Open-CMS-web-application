37c591718f2521cb0e8c96a1304750d062100895
mark brown <mark.russellbrown@gmail.com>
2025-11-27 21:00:14 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_ecology.js
+++ APEX_STATIC_FILE.101.deploy_ecology.js
@@ -11,8 +11,9 @@ export const init = async (e) => {
     
     let config = {method: e.dataset.method, headers: headers};
     const lcp = sessionStorage.getItem("LCP");
+    const fcp = sessionStorage.getItem("FCP");
     const metrics = JSON.parse(sessionStorage.getItem("metrics"));
-    config["body"] = JSON.stringify({metrics:metrics,lcp:lcp});
+    config["body"] = JSON.stringify({metrics:metrics,lcp:lcp,fcp:fcp});
 
     const response = await fetch(url, config);
     const result = await response.json();
