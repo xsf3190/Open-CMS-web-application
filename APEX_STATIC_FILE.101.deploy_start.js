97f417b24135a3f8e95d996edab173f659b04924
mark brown <mark.russellbrown@gmail.com>
2025-11-27 21:00:36 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_start.js
+++ APEX_STATIC_FILE.101.deploy_start.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', async()=>{
     document.body.dataset.jslib = "#JSLIB#";
     document.body.dataset.resturl = "#RESTURL#";
+    
+    const importmap = document.body.dataset.jslib + "importmap.json";
+    console.log("Create importmap " + importmap);
+    const response = await fetch(importmap);
+    const data = await response.json();
+    const im = document.createElement('script');
+    im.type = 'importmap';
+    im.textContent = JSON.stringify(data);
+    document.head.appendChild(im);
+
     const script = document.createElement('script');
     script.src = document.body.dataset.jslib + "javascript/deploy_main.min.js";
     script.type = "module";
