5733628863b87d84897dbb4b0e7b467356fb503b
mark brown <mark.russellbrown@gmail.com>
2025-11-29 21:00:15 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

---
+++ APEX_STATIC_FILE.101.deploy_edit-collection.js
@@ -0,0 +1,57 @@
+/* ************************** */
+/* EDIT COLLECTION INDEX      */
+/* ************************** */
+import { callAPI, handleError } from "deploy_callAPI";
+import { output_dialog, dialog_footer, initDialog } from "deploy_elements";
+
+let endpoint;
+let parentid;
+
+export const init = (element) => {
+    endpoint = element.dataset.endpoint;
+    parentid = element.dataset.parentid;
+    
+    callAPI(endpoint, "GET", "?parentid=" + parentid)
+    .then((data) => {
+        initDialog(data);
+    })
+    .catch((error) => {
+            handleError(error);
+    });
+}
+
+let isSending = false;
+
+const clickHandler = (e) => {
+    if (!e.target.matches(".send")) return;
+
+    if (isSending) {
+        console.log("Prevent double sends");
+        return;
+    }
+    const live=dialog_footer.querySelector("[aria-live]");
+    const loader = dialog_footer.querySelector(".loader");
+
+    isSending = true;
+    live.textContent = e.target.dataset.processing;
+
+    const form = output_dialog.querySelector("form");
+    const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData);
+    loader.style.opacity=1;
+
+    callAPI(endpoint, 'POST', formObj)
+        .then((data) => {
+            isSending = false;
+            loader.style.opacity=0;        
+            live.replaceChildren();
+            live.insertAdjacentHTML('beforeend',data.link);
+            live.style.color = "green";
+        })
+        .catch((error) => {
+            loader.style.opacity=0;
+            handleError(error);
+        });
+};
+
+dialog_footer.addEventListener("click", clickHandler);
