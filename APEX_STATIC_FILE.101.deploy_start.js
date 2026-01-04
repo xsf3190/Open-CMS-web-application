b979765cb11acabe749b92864c59346aa681f486
mark brown <mark.russellbrown@gmail.com>
2026-01-04 21:00:36 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_start.js
+++ APEX_STATIC_FILE.101.deploy_start.js
@@ -1,3 +1,44 @@
+/* 
+** LIGHT DARK BUTTON HANDLERS
+*/
+class ColorScheme extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      this.setupControl("appearance", document.documentElement);
+    }
+
+    handleChange(e, prop, el) {
+      const attr = `data-${prop}`;
+      const value = e.target.value;
+
+      if (!value) {
+        localStorage.removeItem(prop);
+        el.removeAttribute(attr);
+        return;
+      }
+
+      localStorage.setItem(prop, value);
+      el.setAttribute(attr, value);
+    }
+    
+    setupControl(prop, el) {
+      const initialValue = localStorage.getItem(prop) || "";
+      const collection = this.querySelectorAll(`[name='${prop}']`);
+
+      for (let item of collection) {
+        item.checked = item.value === initialValue;
+        item.addEventListener("change", (e) => this.handleChange(e, prop, el));
+      }
+    }
+}
+
+if ("customElements" in window) {
+    window.customElements.define("color-scheme", ColorScheme);
+}
+
 document.addEventListener('DOMContentLoaded', async()=>{
     document.body.dataset.jslib = "#JSLIB#";
     document.body.dataset.resturl = "#RESTURL#";
