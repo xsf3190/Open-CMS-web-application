39afd403ea275defa550682fc0f43ad6e8112d3f
mark brown <mark.russellbrown@gmail.com>
2026-03-25 21:00:33 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

---
+++ APEX_STATIC_FILE.101.deploy_favicon.js
@@ -0,0 +1,42 @@
+/*
+**  WEBSITE FAVICON
+*/
+import { dialog_article, dialog_footer, initDialog, footerHandler, liveRegion } from "deploy_elements";
+import { callAPI, handleError } from "deploy_callAPI";
+
+const favicon = document.querySelector("head > link[rel='icon']");
+
+let endpoint;
+
+/*
+**  BUTTON HANDLER
+*/
+const clickHandler = (e) => {
+    if (!e.target.id === "update") return;
+
+    const value = document.getElementById("favicon").value;
+
+    callAPI(endpoint,"PUT",{favicon:value})
+        .then((data) => {
+            if (data.href) {
+                favicon.setAttribute("href", data.href); 
+            }
+        
+            liveRegion(data);
+        })
+}
+
+
+export const init = (element) => {
+    endpoint = element.dataset.endpoint;
+
+    callAPI(endpoint,"GET")
+        .then((data) => {
+            initDialog(data);
+            dialog_footer.addEventListener("click", footerHandler);
+            dialog_article.addEventListener("click", clickHandler);
+        })
+        .catch((error) => {
+            handleError(error);
+        });
+}
