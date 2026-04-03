4e42b66a10ee6de740f553040e999d09be1cb633
mark brown <mark.russellbrown@gmail.com>
2026-04-03 22:00:52 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_page-options.js
+++ APEX_STATIC_FILE.101.deploy_page-options.js
@@ -1,11 +1,11 @@
-/* ************************** */
-/* EDIT COLLECTION INDEX      */
-/* ************************** */
+/*
+**  GENERIC OPTIONS MODULE. FUNCTIONALITY DEPENDS ON PAGE TYPE - BLOG POST, NORMAL PAGE, ETC
+*/
+
+import { dialog_article, dialog_footer, initDialog, footerHandler, liveRegion } from "deploy_elements";
 import { callAPI, handleError } from "deploy_callAPI";
-import { dialog_article, dialog_footer, initDialog } from "deploy_elements";
 
 let endpoint;
-let dml;
 
 export const init = (element) => {
     endpoint = element.dataset.endpoint;
@@ -13,100 +13,42 @@ export const init = (element) => {
     callAPI(endpoint, "GET")
     .then((data) => {
         initDialog(data);
-        dml = dialog_article.querySelector("[name=dml]")?.value;
+        dialog_footer.addEventListener("click", footerHandler);
+        dialog_article.addEventListener("change", changeHandler);
+        dialog_article.addEventListener("input", inputHandler);
+        dialog_article.addEventListener("click", clickHandler);
     })
     .catch((error) => {
-            handleError(error);
+        handleError(error);
     });
-}
-
-/*
-** SET IMAGE BORDER RADIUS
-*/
-const setImgBorderRadius = (value) => {
-    document.documentElement.style.setProperty('--img-border-radius', value + "px"); 
 }
 
 const changeHandler = (e) => {
-    if (dml==="insert") return;
-
     const name = e.target.getAttribute("name");
-
-    if (name==="logo") {
-        setLogo(e.target.value);
-    }
-    if (name==="logo-inline-size") {
-        setLogoWidth(e.target.value);
-    }
-    if (name==="favicon") {
-        setFavicon(e.target.value);
-    }
-    if (name==="img-border-radius") {
-        setImgBorderRadius(e.target.value);
-    }
     
     callAPI(endpoint, "PUT", {name:name, value:e.target.value})
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-            handleError(error);
-    });
-}
-
-let isSending = false;
-
-/*
-**  CLICK PUBLISH OR DELETE BUTTON
-*/
-const clickHandler = (e) => {
-    if (!e.target.classList.contains("publish") && !e.target.classList.contains("delete")) {
-        return;
-    }
-
-    if (isSending) {
-        console.log("Prevent double sends");
-        return;
-    }
-    const live=dialog_footer.querySelector("[aria-live]");
-    const loader = dialog_footer.querySelector(".loader");
-
-    isSending = true;
-    live.textContent = e.target.dataset.processing;
-    loader.style.opacity=1;
-
-    const formObj = {};
-    let method;
-
-    if (e.target.classList.contains("publish")) {
-        formObj["dml"]=dml;
-        formObj["title"]=dialog_article.querySelector("[name=title]")?.value;
-        formObj["excerpt"]=dialog_article.querySelector("[name=excerpt]")?.value;
-        method = "POST";
-    } else {
-        method="DELETE";
-        e.target.remove();
-    }
-
-    callAPI(endpoint, method, formObj)
         .then((data) => {
-            isSending = false;
-            loader.style.opacity=0;        
-            live.replaceChildren();
-            if (data.link) {
-                live.insertAdjacentHTML('beforeend',data.link);
-                live.style.color = "green";
-            }
-            if (data.message) {
-                live.insertAdjacentHTML('beforeend',data.message);
-                live.style.color = "red";
-            }
+            liveRegion(data);
         })
         .catch((error) => {
-            loader.style.opacity=0;
             handleError(error);
         });
+}
+
+/*
+**  DOES NOTHING !
+*/
+const clickHandler = () => {
+    console.log("clickHandler");
 };
 
-dialog_article.addEventListener("change", changeHandler);
-dialog_footer.addEventListener("click", clickHandler);
+/*
+**  INPUT HANDLER SHOWS USER WHAT THEY'VE DONE
+*/
+const inputHandler = (e) => {
+    const id = e.target.getAttribute("id");
+
+    if (id==="max-width") {
+        document.documentElement.style.setProperty(`--${id}`, e.target.value + "px");  
+    }
+};
