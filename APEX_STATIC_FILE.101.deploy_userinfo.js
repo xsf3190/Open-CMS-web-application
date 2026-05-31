9ae7239eec8f0bf0a7fb8eccbf86613b0d4f4b95
mark brown <mark.russellbrown@gmail.com>
2026-05-31 23:00:54 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_userinfo.js
+++ APEX_STATIC_FILE.101.deploy_userinfo.js
@@ -25,39 +25,7 @@ export const clickHandler = async (e) => {
                 handleError(error);
             });
     }
-    if (e.target.classList.contains("download")) {
-        downloadWebsite();
-    }
 };
-
-/*
-**  DOWNLOAD WEBSITE TO USER'S DOWNLOAD FOLDER
-*/
-let isSending = false;
-
-const downloadWebsite = () => {
-    if (isSending) {
-        console.log("Prevent double sends");
-        return;
-    }
-    const live=dialog_footer.querySelector("[aria-live]");
-    const loader = dialog_footer.querySelector(".loader");
-
-    isSending = true;
-    live.textContent = "Downloading website ... wait a few seconds";
-    loader.style.opacity=1;
-
-    callAPI("download-website/:ID","GET")
-        .then((data) => {
-            isSending = false;
-            loader.style.opacity=0;        
-            live.replaceChildren();
-            live.insertAdjacentHTML('beforeend',data.message);
-        })
-        .catch((error) => {
-            handleError(error);
-        });
-}
 
 export const init = () => {
     callAPI("userinfo/:ID","GET")
