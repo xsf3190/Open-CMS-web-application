9546096a4b72a9808166355db273acd0206e6eaf
mark brown <mark.russellbrown@gmail.com>
2026-06-03 23:00:53 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_publish-website.js
+++ APEX_STATIC_FILE.101.deploy_publish-website.js
@@ -1,9 +1,9 @@
 /*
-** USER CLICKS PUBLISH BUTTON
+** USER CLICKS PUBLISH BUTTON - PUBLISH EDITOR WEBSITE
 */
 
 import { callAPI, handleError } from "deploy_callAPI";
-import { initDialog } from "deploy_elements";
+import { initDialog, dialog_footer } from "deploy_elements";
 
 let endpoint;
 
@@ -16,4 +16,36 @@ export const init = (e) => {
         .catch((error) => {
             handleError(error);
         });;
+}
+
+let isSending = false;
+
+export const clickHandler = (e) => {
+    if (e.target.classList.contains("publish-editor-site")) {
+        if (isSending) {
+            console.log("Prevent double sends");
+            return;
+        }
+        const live=dialog_footer.querySelector("[aria-live]");
+        const loader=dialog_footer.querySelector(".loader");
+
+        isSending = true;
+        live.textContent = e.target.dataset.processing;
+        loader.style.opacity=1;
+
+        callAPI("publish-website/:ID", "POST", {})
+            .then((data) => {
+                isSending = false;
+                loader.style.opacity=0;        
+                live.replaceChildren();
+                live.textContent = data.message;
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1500);
+            })
+            .catch((error) => {
+                loader.style.opacity=0;
+                handleError(error);
+            });
+    }
 }
