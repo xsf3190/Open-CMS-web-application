9a34d9f5aba425463c1acefc7daf1a3c0b5bb702
mark brown <mark.russellbrown@gmail.com>
2026-07-20 22:00:22 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edit-content.js
+++ APEX_STATIC_FILE.101.deploy_edit-content.js
@@ -469,7 +469,7 @@ const saveData = async ( data, endpoint, id ) => {
         img_src: images.map(img => img.getAttribute('src')),
         a11y_alt_fails: images.filter(img => !img.hasAttribute('alt') || !img.getAttribute('alt')).length,
         elements_used: getElementsUsed(ele), 
-        title: ele.querySelector("h2,h3,h4")?.textContent,
+        title: ele.querySelector("h1,h2,h3,h4")?.textContent,
         excerpt: ele.querySelector("p")?.textContent,
     };
 
