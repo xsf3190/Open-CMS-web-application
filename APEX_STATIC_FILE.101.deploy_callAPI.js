f717fcbe958b020bca08aa488348bd1747861140
mark brown <mark.russellbrown@gmail.com>
2026-07-23 22:00:16 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_callAPI.js
+++ APEX_STATIC_FILE.101.deploy_callAPI.js
@@ -1,4 +1,4 @@
-import { bodydata, output_dialog, dialog_article, dialog_footer } from "deploy_elements";
+import { bodydata, output_dialog, dialog_header, dialog_article, dialog_footer } from "deploy_elements";
 
 let access_token = sessionStorage.getItem("token");
 let refresh_token = localStorage.getItem("refresh");
@@ -12,42 +12,22 @@ const handleError = (error) => {
     if (!output_dialog.open) {
         output_dialog.showModal();
     }
-    let logout = false;
-    dialog_article.replaceChildren();
-    let message = "<h4>" + error.name + ": " + error.message + "</h4>";
-    if (message.includes("Unauthorized")) {
-        console.log("refresh_token",refresh_token);
-        console.log("locatStorage",localStorage.getItem("refresh"));
-        sessionStorage.clear();
-        localStorage.clear();
-        message += "<h4>Security precaution against theft</h4>";
-        message += "<p>You are being logged out because you have not authenticated from this location or device. Possible causes:</p>";
-        message += "<ol>";
-        message += "<li>You've connected to a different network</li>";
-        message += "<li>You upgraded or changed browser</li>";
-        message += "<li>Your device was stolen</li>";
-        message += "</ol>";
-        message += "<hr>";
-        message += "<p>Log in again</p>";
-        logout = true;
+    let last = dialog_header.lastChild;
+    while (true) {
+        if (dialog_header.firstChild == last) break;
+        dialog_header.removeChild(dialog_header.firstChild);
     }
-    dialog_article.insertAdjacentHTML('afterbegin',message);
-    
+    dialog_header.insertAdjacentHTML('afterbegin',"<h4>Unexpected error occurred</h4>");
+    dialog_article.replaceChildren();
+    dialog_article.insertAdjacentHTML('afterbegin',"<h4>" + error.name + ": " + error.message + "</h4>");
     dialog_footer.replaceChildren();
     dialog_footer.insertAdjacentHTML('afterbegin',"<span></span>");
-    
-    if (logout) {
-        output_dialog.addEventListener("close", () => {
-            window.location.reload();
-        });
-    }
 }
 
 /* 
 ** CHECK IF TOKEN EXPIRED 
 */
 const expiredToken = (token) => {
-    console.log("Starting expiredToken");
     if (!token) return true;
     const now = Math.floor(new Date().getTime() / 1000);
     const arrayToken = token.split(".");
@@ -67,23 +47,6 @@ const replaceTokens = (data) => {
 }
 
 /* 
-** COMMON ERROR HANDLER FOR API CALLS
-*/
-const responseok = (response, result) => {
-    if (response.ok && result.success) {
-        return(true);
-    }
-    if (result.cause) {
-      throw new Error(`${response.status} - ${result.cause}`);
-    }
-    if (result.sqlcode) {
-      throw new Error(`${response.status} - ${result.sqlerrm}`);
-    } else {
-      throw new Error(`${result.message}`);
-    }
-}
-
-/* 
 ** CALL BACKEND DATABASE API. AUTHENTICATION USES ROTATING JWT TOKENS
 */
 const callAPI = async (endpoint, method, data) => {
@@ -95,6 +58,8 @@ const callAPI = async (endpoint, method, data) => {
     headers.append("url", window.location.hostname);
     headers.append("timezone", Intl.DateTimeFormat().resolvedOptions().timeZone);
 
+    let authenticated = true;
+
     /* No tokens involved in Authenticate endpoints. */
     if (!endpoint.includes("authenticate") && !endpoint.includes("auth-verify")) {
         /* Replace refresh and access tokens when access token expired */
@@ -103,27 +68,36 @@ const callAPI = async (endpoint, method, data) => {
             const refresh_config = {method: "GET", headers: headers};
             try {
                 const url = bodydata.resturl + "refresh-token/" + bodydata.websiteid;
+                
                 const response = await fetch(url, refresh_config);
-                if (!response.ok) {
-                    throw new Error(`Response status: ${response.status}`);
-                }
                 const result = await response.json();
-                if (result.cause) {
-                    throw new Error(`${response.status} - ${result.cause}`);
+
+                if (response.ok) {
+                    replaceTokens(result);
+                } else {
+                    if (response.status === 401) {
+                        authenticated = false;
+                        localStorage.clear();
+                        refresh_token = "";
+                        sessionStorage.clear();
+                        access_token = "";
+                        output_dialog.addEventListener("close", () => {
+                            window.location.reload();
+                        });
+                    }
+                    const error = result.sqlerrm || result.cause || result['o:errorCode'] + " - " + result.message;
+                    throw new Error(`${response.status} - ${error}`);
                 }
-                if (result.sqlcode) {
-                    throw new Error(`${response.status} - ${result.sqlerrm}`);
-                }
-                replaceTokens(result);
             } catch (e) {
                 handleError(e);
             }
         }
     }
 
+    if (!authenticated) return;
+
     /* Call API passing access token */
-    const path = endpoint.replace(":ID",bodydata.websiteid)
-                         .replace(":PAGE",bodydata.articleid);
+    const path = endpoint.replace(":ID",bodydata.websiteid).replace(":PAGE",bodydata.articleid);
     
     let url = bodydata.resturl + path;
   
@@ -140,21 +114,13 @@ const callAPI = async (endpoint, method, data) => {
 
     try {
         const response = await fetch(url, config);
-        console.log("response",response);
-
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
         const result = await response.json();
-        console.log("result",result);
+
+        if (response.ok) return(result);
+
+        const error = result.sqlerrm || result.cause || result['o:errorCode'] + " - " + result.message;
+        throw new Error(`${response.status} - ${error}`);
         
-        if (result.cause) {
-            throw new Error(`${response.status} - ${result.cause}`);
-        }
-        if (result.sqlcode) {
-            throw new Error(`${response.status} - ${result.sqlerrm}`);
-        }
-        return(result);
     } catch (e) {
         handleError(e);
     }
