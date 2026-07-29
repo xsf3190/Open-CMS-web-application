e3fa1dcb3f2b21a93885adf232e2660c0860494e
mark brown <mark.russellbrown@gmail.com>
2026-07-29 22:00:24 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edit-collections.js
+++ APEX_STATIC_FILE.101.deploy_edit-collections.js
@@ -4,7 +4,7 @@
 import { dialog_article, dialog_footer, initDialog, liveRegion } from "deploy_elements";
 import { callAPI } from "deploy_callAPI";
 
-let endpoint, isSending;
+let endpoint, select, collectionitemdata;
 
 export const init = (e) => {
     endpoint = e.dataset.endpoint;
@@ -12,7 +12,8 @@ export const init = (e) => {
     callAPI(endpoint,'GET')
         .then((data) => {
             initDialog(data);
-            isSending = false;
+            select = document.getElementById("collection-list");
+            collectionitemdata = document.getElementById("collectionitemdata");
         })
 }
 
@@ -32,11 +33,11 @@ export const changeHandler = (e) => {
     if (e.target.matches("[name='page']")) {
         console.log("changeHandler",e.target);
     }
+    // User select new collection item
     if (e.target.matches("#collection-list")) {
         const collection_item = e.target.options[e.target.selectedIndex]?.getAttribute("value");
         callAPI(endpoint,'GET',"?collection_item="+collection_item)
         .then((data) => {
-            const collectionitemdata = document.getElementById("collectionitemdata");
             collectionitemdata.replaceChildren();
             collectionitemdata.insertAdjacentHTML('afterbegin',data.item);
             dialog_footer.querySelector(".upd-item").dataset.collectionItem = collection_item;
@@ -59,14 +60,18 @@ export const clickHandler = async (e) => {
         };
         callAPI(endpoint,'POST',obj)
         .then((data) => {
-            // new_is is set when item successfully added
+            // new_id is set when item successfully added
             if (data.new_id) {
-                const select = document.getElementById("collection-list");
+                // Add new title to select list
+                
                 const option = document.createElement("option");
                 option.value = data.new_id;
                 option.text = obj["title"];
                 select.add(option,select.options[0]);
                 select.querySelector("selectedcontent").textContent = obj["title"];
+                collectionitemdata.replaceChildren();
+                collectionitemdata.insertAdjacentHTML('afterbegin',data.item);
+                dialog_footer.querySelector(".upd-item").dataset.collectionItem = collection_item;
             } else {
                 // otherwise signal error
                 title.setAttribute("aria-invalid", "true");
@@ -91,31 +96,29 @@ export const clickHandler = async (e) => {
         });
     }
 
-    if (e.target.matches(".save")) {
-        /* SAVE CHANGES AND PROMPT USER TO PUBLISH */
-
-        if (isSending) {
-            console.log("Prevent double sends");
-            return;
-        }
-        const live=dialog_footer.querySelector("[aria-live]");
-        const loader = dialog_footer.querySelector(".loader");
-        
-        isSending = true;
-        live.textContent = e.target.dataset.processing;
-        loader.style.opacity=1;
-
-        await callAPI(endpoint,'POST', {})
-            .then((data) => {
-                isSending = false;
-                loader.style.opacity=0;        
-                live.replaceChildren();
-                live.textContent = data.message;
-                if (data.published) {
-                    setTimeout(() => {
-                        window.location.reload();
-                    }, 1500);
+    if (e.target.matches(".del-item")) {
+        const collectionItem = e.target.dataset.collectionItem;
+        const obj = {
+            action: "delete",
+            collection_item: collectionItem
+        };
+        callAPI(endpoint,'POST',obj)
+        .then((data) => {
+            for (let i = 0; i < select.options.length; i++) {
+                if (select.options[i].value === collectionItem) {
+                    select.remove(i);
                 }
-            })
+            }
+            document.getElementById("excerpt-upd").value = "";
+            dialog_article.querySelectorAll("[name='link']:checked").forEach(item => {
+                item.checked = false;
+            });
+            const del_btn = dialog_article.querySelector(".del-item");
+            del_btn.dataset.collectionItem = 0;
+            del_btn.previousSibling.textContent = "";
+            dialog_footer.querySelector(".upd-item").dataset.collectionItem = 0;
+            liveRegion(data);
+            select.focus();
+        });
     }
 }
