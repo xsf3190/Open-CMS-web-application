5db167060a98de6b102d51f451e8b30d5dca36ba
mark brown <mark.russellbrown@gmail.com>
2026-08-10 23:00:27 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edit-styles.js
+++ APEX_STATIC_FILE.101.deploy_edit-styles.js
@@ -4,7 +4,7 @@
 import { dialog_article, dialog_footer, initDialog, liveRegion } from "deploy_elements";
 import { callAPI } from "deploy_callAPI";
 
-let endpoint;
+let endpoint, style_id;
 
 export const init = (e) => {
     endpoint = e.dataset.endpoint;
@@ -19,14 +19,39 @@ export const init = (e) => {
 ** INPUT HANDLER
 */
 export const inputHandler = (e) => {
-    console.log("DO NOTHING");
+    if (e.target.tagName==="INPUT" && e.target.type==="range") {
+        document.documentElement.style.setProperty(`--${e.target.id}`, `${e.target.value}px`);
+    }
 }
 
 /*
 **  CHANGE HANDLER
 */
 export const changeHandler = (e) => {
-    console.log("DO NOTHING");
+    if (e.target.matches("#styles")) {
+        style_id = e.target.options[e.target.selectedIndex].getAttribute("value");
+        if (style_id) {
+            callAPI(endpoint,'GET',"?style="+style_id)
+            .then((data) => {
+                const control = e.target.nextElementSibling;
+                control.replaceChildren();
+                control.insertAdjacentHTML('afterbegin',data.input);
+                control.querySelector("input").focus();
+            });
+        }
+        return;
+    }
+    
+    if (e.target.tagName==="INPUT" && e.target.type==="color") {
+        document.documentElement.style.setProperty(`--${e.target.id}`, e.target.value);
+    }
+
+    if (e.target.tagName==="INPUT") {
+        callAPI(endpoint,'POST',{style_id: style_id, property:e.target.id, value:e.target.value})
+            .then((data) => {
+                liveRegion(data);
+            });
+    }
 }
 
 /*
